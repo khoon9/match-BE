112619ff2ac144c8d2ee601bfdb22067f0624f60
khoon9 <kshun08@dongguk.edu>
2023-12-12 22:52:17 +0900

    fix: 방문자 등록 유효성 검사 추가
    이미 등록된 UUID 에 대한 중복 등록 방지

--- src/main/java/com/platform/match/service/ClientVisitorService.java
+++ src/main/java/com/platform/match/service/ClientVisitorService.java
@@ -19,19 +19,22 @@ public class ClientVisitorService {
     private final ClientVisitorRepository clientVisitorRepository;
 
     public String saveClientVisitor(ClientVisitorDto clientVisitorDto) {
-        clientVisitorRepository.save(clientVisitorDto.toEntity());
-        return "저장 성공";
+        if(clientVisitorRepository.getReferenceByUuid(clientVisitorDto.uuid())==null) {
+            clientVisitorRepository.save(clientVisitorDto.toEntity());
+            return "저장 성공";
+        } else {
+            return "이미 해당 방문자 등록되어 있음";
+        }
     }
 
     public String updateClientVisitor(String uuid){
         try{
             ClientVisitor clientVisitor = clientVisitorRepository.getReferenceByUuid(uuid);
             clientVisitor.setVisitCount(clientVisitor.getVisitCount() + 1);
-            return "업데이트 성공";
         }catch (EntityNotFoundException e){
             log.warn("방문자 업데이트 실패. 해당 방문자를 찾을 수 없습니다 - uuid: {}", uuid);
             return "업데이트 실패";
         }
-
+        return "업데이트 성공";
     }
 }
