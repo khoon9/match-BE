0bc004effb484f0d4dd1bc8b596c7f7cf7a02bbe
khoon9 <kshun08@dongguk.edu>
2023-12-29 16:15:50 +0900

    feat: Trial - 03 Asia/Seoul 표준 시간대로 조정

--- scripts/start.sh
+++ scripts/start.sh
@@ -15,7 +15,7 @@ echo "[$NOW] $JAR 복사" >> $START_LOG
 cp $ROOT_PATH/build/libs/match-1.0.0.jar $JAR
 
 echo "[$NOW] > $JAR 실행" >> $START_LOG
-nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
+nohup java -Duser.timezone=Asia/Seoul -jar $JAR > $APP_LOG 2> $ERROR_LOG &
 
 SERVICE_PID=$(pgrep -f $JAR)
 echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
